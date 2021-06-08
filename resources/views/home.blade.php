4cfd1f39425e3c360ca221a4cace30ec6e88ae71
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-06-09 01:05:36 +0200

    Prima parte main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,35 @@
     {{-- MAIN --}}
     <main>
 
-        
+        {{-- Wrapper --}}
+        <div class="wrapper">
+
+            {{-- Title --}}
+            <div class="main-title">
+                <h1>Current Series</h1>
+            </div>
+
+           <div class="comics-container">
+
+                 {{-- Single Comic Series --}}
+            
+            @foreach ($comics_array as $comic)
+            <div class="single-comic">
+                <img src="{{ $comic['thumb'] }}" alt="">
+                <h2> {{ $comic['title'] }} </h2>
+            </div>
+            @endforeach
+
+           </div>
+            
+
+        </div>
+
+        {{-- Load More Button --}}
+        <div class="load-more">
+            <a href="">Load More</a>
+        </div>
+
     </main>
 
     
