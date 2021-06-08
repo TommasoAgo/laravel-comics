aaa905cb81eb5cbf6a33ba0229a765270c252375
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-06-09 00:07:35 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('tag_title')
+    Comics Homepage
+@endsection
+
+@section('main_section')
+    
+    {{-- MAIN --}}
+    <main>
+
+        
+    </main>
+
+    
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,97 @@
+{{-- HEADER --}}
+
+<header>
+
+    {{-- Header upper section --}}
+    <div class="header-top-line">
+        <nav class="wrapper">
+            <ul class="header-top-menu">
+                <li>
+                   <a href="">Dc Power <span>TM</span> Visa</a>  
+                </li>
+
+                <li>
+                    <a href="">Additional DC Sites<i class="fas fa-caret-down"></i></a>  
+                 </li>
+            </ul>
+        </nav>
+    </div>
+
+    {{-- Header main section --}}
+    <div class="header-main-section">
+        <nav class="header-main-container wrapper">
+
+            {{-- Logo --}}
+            <div class="header-logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="Logo">
+            </div>
+
+            {{-- Header Main Menu --}}
+            <ul class="header-main-menu">
+
+                {{-- Characters --}}
+                <li>
+                   <a href="">Characters</a> 
+                </li>
+
+                {{-- Comics --}}
+                <li>
+                    <a href="">Comics</a> 
+                 </li>
+
+                 {{-- Movies --}}
+                 <li>
+                    <a href="">Movies</a> 
+                 </li>
+
+                 {{-- TV --}}
+                 <li>
+                    <a href="">TV</a> 
+                 </li>
+
+                 {{-- Games --}}
+                 <li>
+                    <a href="">Games</a> 
+                 </li>
+
+                 {{-- Collectibles --}}
+                 <li>
+                    <a href="">Collectibles</a> 
+                 </li>
+
+                 {{-- Videos --}}
+                 <li>
+                    <a href="">Videos</a> 
+                 </li>
+
+                 {{-- Fans --}}
+                 <li>
+                    <a href="">Fans</a> 
+                 </li>
+
+                 {{-- News --}}
+                 <li>
+                    <a href="">News</a> 
+                 </li>
+
+                 {{-- Shop --}}
+                 <li>
+                    <a href="">Shop<i class="fas fa-caret-down"></i></a> 
+                 </li>
+            </ul>
+
+            {{-- Input Search --}}
+            <div class="input-search">
+                <input type="search" name="search" id="search" placeholder="Search">
+                <i class="fas fa-search"></i>
+            </div>
+        </nav>
+    </div>
+
+    {{-- Jumbotron --}}
+    <div class="jumbotron">
+         
+    </div>
+
+
+</header>
