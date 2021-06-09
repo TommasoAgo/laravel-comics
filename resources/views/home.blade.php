5b35fb2b7294fbabe09e9fc75787da28cf7801e0
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-06-09 15:42:47 +0200

    Finito di fare il Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,35 +9,77 @@
     {{-- MAIN --}}
     <main>
 
-        {{-- Wrapper --}}
-        <div class="wrapper">
+        <div class="main-first-section">
 
-            {{-- Title --}}
-            <div class="main-title">
-                <h1>Current Series</h1>
+            {{-- Wrapper --}}
+            <div class="wrapper">
+
+                {{-- Title --}}
+                <div class="main-title">
+                    <h1>Current Series</h1>
+                </div>
+
+                <div class="comics-container">
+
+                    {{-- Single Comic Series --}}
+                
+                @foreach ($comics_array as $comic)
+                <div class="single-comic">
+                    <img src="{{ $comic['thumb'] }}" alt="">
+                    <h2> {{ $comic['title'] }} </h2>
+                </div>
+                @endforeach
+
+                </div>
+                
+
+                </div>
+
+                {{-- Load More Button --}}
+                <div class="load-more">
+                    <a href="">Load More</a>
+                </div>
+
             </div>
 
-           <div class="comics-container">
+            <div class="main-second-section">
 
-                 {{-- Single Comic Series --}}
-            
-            @foreach ($comics_array as $comic)
-            <div class="single-comic">
-                <img src="{{ $comic['thumb'] }}" alt="">
-                <h2> {{ $comic['title'] }} </h2>
+                {{-- Wrapper --}}
+                <div class="main-menu wrapper">
+
+                    {{-- Second Section Menu --}}
+
+                    {{-- Digital Comics --}}
+                    <div class="main-menu-single-el">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+                        <a href="">Digital Comics</a>
+                    </div>
+
+                    {{-- DC Merchandise --}}
+                    <div class="main-menu-single-el">
+                        <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="">
+                        <a href="">DC Merchandise</a>
+                    </div>
+
+                    {{-- Subscription --}}
+                    <div class="main-menu-single-el">
+                        <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="">
+                        <a href="">Subscription</a>
+                    </div>
+
+                    {{-- Comic Shop Locator --}}
+                    <div class="main-menu-single-el">
+                        <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="">
+                        <a href="">Comic Shop Locator</a>
+                    </div>
+
+                    {{-- DC Power VISA --}}
+                    <div class="main-menu-single-el">
+                        <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="">
+                        <a href="">DC Power VISA</a>
+                    </div>
+                </div>
             </div>
-            @endforeach
-
-           </div>
-            
-
-        </div>
-
-        {{-- Load More Button --}}
-        <div class="load-more">
-            <a href="">Load More</a>
-        </div>
-
     </main>
 
     
